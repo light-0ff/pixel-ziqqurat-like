685adc111d07d8f67f92f966bd129cb7ff3f8ee4
light-0ff <tortles777@gmail.com>
2024-01-25 22:21:45 +0200

    Feature: setup window/ camera/ center - the starting point

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,47 @@
 
 use bevy::prelude::*;
 
+pub const WINDOW_WIDTH: f32 = 1280.0;
+pub const WINDOW_HEIGHT: f32 = 800.0;
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
-        .add_systems(Startup, setup)
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "Some game window".into(),
+                        position: WindowPosition::Centered(MonitorSelection::Primary),
+                        resolution: (WINDOW_WIDTH, WINDOW_HEIGHT).into(),
+                        resizable: false,
+                        ..default()
+                    }),
+                    ..default()
+                })
+                .set(ImagePlugin::default_nearest()),
+        )
+        .add_systems(Startup, (setup, spawn_center))
         .run();
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(Camera2dBundle::default());
-    commands.spawn(SpriteBundle {
-        texture: asset_server.load("ducky.png"),
-        ..Default::default()
-    });
+fn setup(mut commands: Commands) {
+    commands
+        .spawn(Camera2dBundle::default())
+        .insert(Name::new("MainCamera"));
+}
+
+pub fn spawn_center(mut commands: Commands) {
+    commands
+        .spawn(SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgb(1.2, 1.2, 1.2),
+                custom_size: Some(Vec2::new(60.0, 60.0)),
+                ..default()
+            },
+            transform: Transform {
+                translation: Vec3::new(0.0, 0.0, 0.0),
+                ..default()
+            },
+            ..default()
+        })
+        .insert(Name::new("Center"));
 }
