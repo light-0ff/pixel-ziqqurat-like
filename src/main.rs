a338a8e86e348b3befc81a44851d572a7bc3e1fd
light-0ff <tortles777@gmail.com>
2024-09-29 16:06:17 +0300

    feature: spawn player

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use bevy::prelude::*;
 
-pub const WINDOW_WIDTH: f32 = 640.0 * 2.0;
-pub const WINDOW_HEIGHT: f32 = 360.0 * 2.0;
+pub const SCALE_MULTIPLIER: f32 = 2.0;
+pub const WINDOW_WIDTH: f32 = 640.0 * SCALE_MULTIPLIER;
+pub const WINDOW_HEIGHT: f32 = 360.0 * SCALE_MULTIPLIER;
 fn main() {
     App::new()
         .add_plugins(
@@ -19,9 +20,39 @@ fn main() {
                 }),
         )
         .add_systems(Startup, setup)
+        .add_systems(Startup, spawn_player)
         .run();
 }
 
 fn setup(mut commands: Commands) {
     commands.spawn((Camera2dBundle::default(), Name::new("MainCamera")));
+}
+
+#[derive(Component)]
+struct Player;
+pub const PLAYER_WIDTH: f32 = 32.0 * SCALE_MULTIPLIER;
+pub const PLAYER_HEIGHT: f32 = 32.0 * SCALE_MULTIPLIER;
+
+fn spawn_player(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlasLayout>>,
+) {
+    let texture = asset_server.load("knight.png");
+    let layout = TextureAtlasLayout::from_grid(UVec2::new(32, 32), 8, 8, None, None);
+    let texture_atlas_layout = texture_atlases.add(layout);
+
+    commands.spawn((
+        SpriteBundle {
+            texture,
+            transform: Transform::from_scale(Vec3::splat(6.0)),
+            ..default()
+        },
+        TextureAtlas {
+            layout: texture_atlas_layout,
+            index: 0,
+        },
+        Player,
+        Name::new("Player"),
+    ));
 }
